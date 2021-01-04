f608fa68300d9ec3695fd75a9a4d678dfe94cd50
drivehostjpc <drivehostjpc@gmail.com>
2021-01-04 12:06:47 +0100

    Afegit 'use strict' a activitat_IT.js

--- js/activitat_IT.js
+++ js/activitat_IT.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let glUltimaPreg, glViRadio, glViCheckBox, glViText, glViSelectOne;
 let glArrRespostes, glArrRespostesCorrectes;
 
@@ -25,8 +27,28 @@ glArrRespostesCorrectes = [glUltimaPreg];
 glArrRespostes=[glViText,glViCheckBox,glViRadio,glViSelectOne];
 glArrRespostesCorrectes=["resp1","0100",2,"Temporibus autem quibusdam et aut officiis debitis"];
 
+function obteSeguentPregunta(cadNumPregActual) {
+  let cadNumPreg;
+  let seccions = document.body.getElementsByTagName('article');
+  let seguentPregunta, seccio;
+    
+    for (let i = 0, length = seccions.length; i < length; i++) {
+      seccio = seccions[i];
+      // seccio = pregunta-01-XX
+      //                   ^^
+      //          01234567890123
+      // indexNumPreg = 
+      cadNumPreg = seccio.id.charAt(9) + seccio.id.charAt(10);
+  
+      if ((cadNumPreg==cadNumPregActual) && (parseInt(cadNumPreg) < glUltimaPreg)){
+        seguentPregunta = seccions[i+1].id.charAt(12) + seccions[i+1].id.charAt(13);
+        break;
+      }
+    } // for (let i = 0, length = seccions.length; i < length; i++) 
+    return seguentPregunta;
+}  // FINAL ==>> function obteSeguentPregunta(cadNumPregActual)
 
-function guardaInputEscollit(objecte){
+function guardaInputEscollit(objecte) {
 /* id => pregunta-01-xx
  * ######         ^^
  * ######01234567890123
@@ -41,7 +63,7 @@ function guardaInputEscollit(objecte){
  */
 
   let objId, intNumPreg, cadNumPreg, indexNumPreg, cadTipusPreg;
-  let colInputs, repostaText, cadRespostes;
+  let colInputs, repostaText, seccio;
 
   objId = objecte.parentElement.id 
   cadNumPreg = objId.charAt(9) + objId.charAt(10);
@@ -51,9 +73,8 @@ function guardaInputEscollit(objecte){
 
   colInputs = objecte.children;
 
-  escollits = "";
   repostaText = "";
-  cadRespostes = "";
+
 
   seccio = document.getElementById("pregunta-" + intNumPreg);
 
@@ -98,7 +119,7 @@ function guardaInputEscollit(objecte){
 
 
 
-function seguentPregunta(objecte){
+function seguentPregunta(objecte) {
 /* id => pregunta-01-xx
  * ######         ^^
  * ######01234567890123
@@ -112,7 +133,9 @@ function seguentPregunta(objecte){
  * cadTipusPreg = id.charAt(12)+id.charAt(13);
  */
 
-  let objId, numPreg, cadNumPreg, indexNumPreg, cadTipusPreg, cadTipusPregSeguent;
+  let objId, numPreg, cadNumPreg, indexNumPreg, cadTipusPreg;
+  let numPregSeguent, cadNumPregSeguent, cadTipusPregSeguent;
+  let valorInicial, pregunta;
 
   objId = objecte.parentElement.id 
   cadNumPreg = objId.charAt(9)+objId.charAt(10);
@@ -127,91 +150,81 @@ function seguentPregunta(objecte){
   
   cadTipusPregSeguent = obteSeguentPregunta(cadNumPreg);
   switch  (cadTipusPreg) {
-    case "tx":
+    case "tx": // INICI case "tx"
       valorInicial = glViText;
-      break; // FINAL case "tx":
-    case "ch":
+      break;   // FINAL case "tx"
+
+    case "ch": // INICI case "ch"
       valorInicial = glViCheckBox;
-      break; // FINAL case "tx":
-    case "ra":
+      break;   // FINAL case "ch":
+
+    case "ra": // INICI case "ra"
       valorInicial = glViRadio;
-      break; // FINAL case "ra":
-    case "so":
+      break;   // FINAL case "ra":
+
+    case "so": // INICI case "so"
       valorInicial = glViSelectOne;
-      break; // FINAL case "so":
+      break;   // FINAL case "so":
   }
 
-  if (glArrRespostes[indexNumPreg]==valorInicial){
-        alert("No hi ha cap seleccionat!");
-      } else { // ELSE if (glArrRespostes[numPreg]==valorInicial)
-        pregunta = document.getElementById("pregunta-" + cadNumPreg + "-" + cadTipusPreg);
-        pregunta.classList.remove("elementVisible");
-        pregunta.classList.add("elementOcult");
-        if (numPreg!=glUltimaPreg) {
-          pregunta = document.getElementById("pregunta-" + cadNumPregSeguent + "-" + cadTipusPregSeguent);
-          pregunta.classList.remove("elementOcult");
-          pregunta.classList.add("elementVisible");
-        } else {  // ELSE --> if (numPreg!=glUltimaPreg)
-          pregunta = document.getElementById("resultat");
-          pregunta.classList.add("elementVisible");
-          pregunta.classList.remove("elementOcult");
-          mostraResultat();
-        } // FINAL --> if (numPreg!=glUltimaPreg) 
-      } // FINAL --> if (glArrRespostes[numPreg]==valorInicial)
-
-}  // FINAL --> function seguentPregunta()
+  if (glArrRespostes[indexNumPreg] == valorInicial){
+    alert("No hi ha cap seleccionat!");
+  } else { // ELSE ==>> if (glArrRespostes[indexNumPreg] == valorInicial)
+    pregunta = document.getElementById("pregunta-" + cadNumPreg + "-" + cadTipusPreg);
+    pregunta.classList.remove("elementVisible");
+    pregunta.classList.add("elementOcult");
+    if (numPreg!=glUltimaPreg) {
+      pregunta = document.getElementById("pregunta-" + cadNumPregSeguent + "-" + cadTipusPregSeguent);
+      pregunta.classList.remove("elementOcult");
+      pregunta.classList.add("elementVisible");
+    } else { // ELSE ==>> if (numPreg!=glUltimaPreg)
+      pregunta = document.getElementById("resultat");
+      pregunta.classList.add("elementVisible");
+      pregunta.classList.remove("elementOcult");
+      mostraResultat();
+    } // FINAL ==>> if (numPreg!=glUltimaPreg) 
+  } // FINAL ==>> if (glArrRespostes[indexNumPreg] == valorInicial)
+} // FINAL ==>> function seguentPregunta(objecte)
 
 
 function mostraResultat() {
+  let resultat,notaFinal;
+  let puntuacioTotal, puntsResposta, textResultat, resultatResposta;
+
   resultat = document.getElementById("resultat");
   notaFinal = document.getElementById("notaFinal");
   resultat.classList.remove("elementOcult");
   resultat.classList.add("elementVisible");
-  // debugger;
-/*
- * glArrRespostes=[-1, -1, -1, -1];
- * glArrRespostesCorrectes=[1, 2, 3, 4];
- */
-  // let textResultat = document.getElementById("textResultat");
-  let puntuacioTotal = 0;
-  let puntsResposta = 0;
-  let textResultat = "";
-  let resultatResposta = "";
+
+  puntuacioTotal = 0;
+  puntsResposta = 0;
+  textResultat = "";
+  resultatResposta = "";
 
   for (let index = 0; index < glArrRespostesCorrectes.length; index++) {
-    resultatResposta = (glArrRespostesCorrectes[index]==glArrRespostes[index]) ? "respOk":"respNok";
-    puntsResposta = (glArrRespostesCorrectes[index]==glArrRespostes[index]) ? "<span class=\"unPunt\">Has obtingut 1 punt!<br></span></span>":"<span class=\"capPunt\">NO has obtingut cap punt!<br></span></span>";
-    puntuacioTotal = (glArrRespostesCorrectes[index]==glArrRespostes[index]) ? puntuacioTotal+1:puntuacioTotal+0;
+    if (glArrRespostesCorrectes[index]==glArrRespostes[index]) {
+      resultatResposta =  "respOk";
+      puntsResposta = "<span class=\"unPunt\">Has obtingut 1 punt!<br></span></span>";
+      puntuacioTotal++; 
+    } else { // ELSE ==>> if (glArrRespostesCorrectes[index]==glArrRespostes[index])
+      resultatResposta =  "respNok";
+      puntsResposta = "<span class=\"capPunt\">NO has obtingut cap punt!<br></span></span>";      
+    } // FINAL ==>> if (glArrRespostesCorrectes[index]==glArrRespostes[index])
 
-    textResultat =  textResultat + "<span class=\"titolResposta\">Pregunta #" + (index + 1) + "</span>" +
-        puntsResposta + 
-        "<span class=\"textResposta\">La resp. corr. és <span class=\"respOk\">" +
-        glArrRespostesCorrectes[index] + "</span> i</span><br>"+
-        "<span class=\"textResposta\">la teva resp. és " +
-        "<span class=\"" + resultatResposta + "\">" +
-        glArrRespostes[index] + "</span></span><br>";
+    textResultat =  textResultat +
+      '<span class="titolResposta">Pregunta #'+
+      (index + 1) +
+      '</span>' +
+      puntsResposta +
+      '<span class="textResposta">La resp. corr. és <span class="respOk">' +
+      glArrRespostesCorrectes[index] +
+      '</span> i</span><br><span class="textResposta">la teva resp. és <span class="' +
+      resultatResposta +
+      '">' +
+      glArrRespostes[index] +
+      '</span></span><br>';
+  } // FINAL for (let index = 0; index < glArrRespostesCorrectes.length; index++)
 
   document.getElementById("textResultat").innerHTML = textResultat;
   notaFinal.innerHTML = "Nota = " + puntuacioTotal + "/" + glUltimaPreg;
-}
-
-function obteSeguentPregunta(cadNumPregActual) {
-let cadNumPreg;
-let seccions = document.body.getElementsByTagName('article');
-let seguentPregunta, seccio;
-  
-  for (let i = 0, length = seccions.length; i < length; i++) {
-    seccio = seccions[i];
-    // seccio = pregunta-01-XX
-    //                   ^^
-    //          01234567890123
-    // indexNumPreg = 
-    cadNumPreg = seccio.id.charAt(9) + seccio.id.charAt(10);
-
-    if ((cadNumPreg==cadNumPregActual) && (parseInt(cadNumPreg) < glUltimaPreg)){
-      seguentPregunta = seccions[i+1].id.charAt(12) + seccions[i+1].id.charAt(13);
-      break;
-    }
-  } // for (let i = 0, length = seccions.length; i < length; i++) 
-  return seguentPregunta;
-}
+} // FINAL ==>> function mostraResultat()
